f4a2ef818263ff4384c6640bfa3d525793572ba9
student-03-e08e876021cb <71582526+student-03-e08e876021cb@users.noreply.github.com>
2023-09-27 15:50:48 +0530

    Update bank_atm.py

--- bank_atm.py
+++ bank_atm.py
@@ -1,4 +1,5 @@
 class atm:
+    total=0
     def withdraw(self,cash,total):
         if cash<total:
             total=total-cash
@@ -18,24 +19,41 @@ class atm:
         print("Balance is ",total)
         print("_______________________")
 
-total=5000
-obj=atm()
-l=int(input("Enter 0 to enter into the atm...."))
+
+obj1=atm()
+obj1.total=5000
+obj2=atm()
+obj2.total=200
+l=0
+x=int(input("Select the atm...1...or...2...->"))
 while(l!=4):
-    print("__________ATM__________")
+    
+    print("__________ATM{}_______".format(x))
     print()
     print("1. Withdrawl\n2. Deposit\n3. Check Balance\n4. Exit")  
     i=int(input("Enter the function required...."))
     if i==1:
         m=int(input("enter the amount need to be withdrawn...."))
-        total=obj.withdraw(m,total)   
+        if x==1:
+            obj1.total=obj1.withdraw(m,obj1.total) 
+        elif x==2:
+            obj2.total=obj2.withdraw(m,obj2.total) 
     elif i==2:
         m=int(input("enter the amount need to be deposited....")) 
-        total=obj.deposit(m,total)  
+        if x==1:
+            obj1.total=obj1.deposit(m,obj1.total) 
+        elif x==2:
+             obj2.total=obj2.deposit(m,obj2.total)
     elif i==3:
-        obj.check_balance(total)
+        if x==1:
+            obj1.check_balance(obj1.total)
+        elif x==2:
+            obj2.check_balance(obj2.total)
     elif i==4:
         l=4
         print("________EXIT___________")
     else:
         print("enter either 1,2,3 or 4.")    
+
+
+
